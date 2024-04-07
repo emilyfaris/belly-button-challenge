748aca36b2247bf0d82a9e1c0866b8a1a3eadb1c
Emily Faris <emilyfaris@gmail.com>
2024-04-07 14:29:11 -0700

    adding bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,51 +1,15 @@
 // Reading the JSON data using D3
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
-d3.json(url).then(init);
+d3.json(url).then(data => {
+    console.log(data);
+    init(data);
+});
 
-// Displaying the default plot
-function init(data) {
-    let firstSample = data.samples[0];
-    let sampleValues = firstSample.sample_values.slice(0, 10);
-    let otuIds = firstSample.otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`);
-    let otuLabels = firstSample.otu_labels.slice(0, 10);
 
-    // Creating the default bar plot
-    let trace1 = {
-        x: sampleValues,
-        y: otuIds,
-        text: otuLabels,
-        type: "bar",
-        orientation: "h"
-    };
+// Initalizing the dashboard
 
-    let layout = {
-        title: "Top 10 OTUs Found",
-    };
 
-    let data1 = [trace1]
-
-    Plotly.newPlot('bar', data1, layout);
-
-    // Populating the dropdown menu
-    let dropdownMenu = d3.select("#selDataset");
-    data.names.forEach((name) => {
-        dropdownMenu.append("option").text(name).property("value", name);
-    });
-
-    // On change to the dropdown, call getData()
-    d3.selectAll("#selDataset").on("change", getData);
-
-    // Function called by dropdown menu changes
-    function getData() {
-        let selectedId = dropdownMenu.property("value");
-        let selectedSample = data.samples.find(sample => sample.id === selectedId);
-        
-        updateBarChart(selectedSample);
-        // Add calls to update other charts here
-    }
-}
-
-// Function to update the bar chart
+// Updating visualizations based on drop down selection
 function updateBarChart(sample) {
     let sampleValues = sample.sample_values.slice(0, 10);
     let otuIds = sample.otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`);
@@ -55,3 +19,52 @@ function updateBarChart(sample) {
     Plotly.restyle('bar', "y", [otuIds]);
     Plotly.restyle('bar', "text", [otuLabels]);
 }
+
+// Defining visualization functions
+    // Bar Chart
+    function updateBarChart(sample) {
+        let trace1 = {
+            x: sample.sample_values.slice(0, 10).reverse(),
+            y: sample.otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            text: sample.otu_labels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        };
+
+        data1 = [trace1]
+    
+        let layout1 = {
+            title: 'Top 10 OTUs Found',
+        };
+    
+        Plotly.newPlot('bar', data1, layout1);
+    }
+
+    // Bubble Chart
+    function updateBubbleChart(sample) {
+        let trace2 = {
+            x: sample.otu_ids,
+            y: sample.sample_values,
+            text: sample.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: sample.sample_values,
+                color: sample.otu_ids,
+                colorscale: 'Earth'
+            }
+        };
+
+        data2 = [trace2]
+    
+        let layout2 = {
+            title: 'Bacteria Cultures Per Sample',
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+    
+        Plotly.newPlot('bubble', data2, layout2);
+    }
+
+    // Metadata
+     
